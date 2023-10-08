0440fd3b21cce08ede214c980826d2a471e0f578
EC2 Default User <ec2-user@ip-172-31-37-154.ap-northeast-1.compute.internal>
2023-10-08 08:50:59 +0000

    [Add] newの更新エラー前まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,14 +22,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
     redirect_to book_path(book.id)
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to '/books'
   end
 
