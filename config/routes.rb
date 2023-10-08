eec95881f2234b211ba56c291d159f933648cc96
EC2 Default User <ec2-user@ip-172-31-37-154.ap-northeast-1.compute.internal>
2023-10-08 03:24:20 +0000

    [Add] index作成まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/new' => 'books#new'
   post 'books' => 'books#create'
-  get '/index' =>'books#index'
+  get '/books' =>'books#index'
   get '/show' => 'books#show'
   get '/edit' => 'books#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
